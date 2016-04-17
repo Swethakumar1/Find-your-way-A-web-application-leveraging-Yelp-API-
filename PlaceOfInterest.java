6050baa618fd16aea596bc2456dba05853f97ffa
Swethakumar1 <swetha010491@gmail.com>
2016-04-17 15:53:26 -0700

    Added files via upload

---
+++ PlaceOfInterest.java
@@ -0,0 +1,47 @@
+package com.process;
+
+public class PlaceOfInterest {
+	public String name;
+	public String address;
+	public String coordinates;
+	public String phoneNumber;	
+	public double rating;
+	public int reviewCount;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCoordinates() {
+		return coordinates;
+	}
+	public void setCoordinates(String coordinates) {
+		this.coordinates = coordinates;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	public int getReviewCount() {
+		return reviewCount;
+	}
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+}
